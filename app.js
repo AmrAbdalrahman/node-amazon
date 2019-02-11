bd94cd96802a8003517d571a9e3f4d0162ef9df1
AmrAlmagic <amr.abdalrahman94@gmail.com>
2019-02-11 22:31:17 +0200

    works with mongoose

--- app.js
+++ app.js
@@ -2,28 +2,34 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 
 const app = express();
 
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/shop',{ useNewUrlParser: true }).then((db)=>{
+    console.log('MONGO connectes');
+
+}).catch(error=> console.log(error));
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
-/*
-const shopRoutes = require('./routes/shop');*/
+const shopRoutes = require('./routes/shop');
 
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findById('5baa2528563f16379fc8a610')
+    User.findById('5c61cbe49fc6621490eb78b6')
         .then(user => {
-            req.user = new User(user.name, user.email, user.cart, user._id);
+            req.user = user;
             next();
         })
         .catch(err => console.log(err));
@@ -31,12 +37,29 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 
-/*
-app.use(shopRoutes);*/
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-    app.listen(3000);
+
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>{
+    User.findOne().then(user => {
+        if (!user) {
+            const user = new User({
+                name: 'Max',
+                email: 'max@test.com',
+                cart: {
+                    items: []
+                }
+            });
+            user.save();
+        }
+
+    });
+    console.log(`listening on port ${port}`);
 });
 
