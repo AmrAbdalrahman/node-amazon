4a1c035e7b68c02bd42d65f925f7673440309819
AmrAlmagic <amr.abdalrahman94@gmail.com>
2019-02-16 16:58:57 +0200

    works with cookie and sessions

--- app.js
+++ app.js
@@ -3,31 +3,54 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MondoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb://localhost:27017/shop';
 
 const app = express();
 
+
+const store = new MondoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+});
+
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/shop',{ useNewUrlParser: true }).then((db)=>{
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true}).then((db) => {
     console.log('MONGO connectes');
 
-}).catch(error=> console.log(error));
+}).catch(error => console.log(error));
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(
+    session({
+        secret: 'magicano',
+        resave: false,
+        saveUninitialized: false,
+        store: store
+    })
+);
+
 app.use((req, res, next) => {
-    User.findById('5c61cbe49fc6621490eb78b6')
+
+    if (!req.session.user) {
+        return next();
+    }
+    User.findById(req.session.user._id)
         .then(user => {
             req.user = user;
             next();
@@ -36,17 +59,15 @@ app.use((req, res, next) => {
 });
 
 app.use('/admin', adminRoutes);
-
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 
-
-
 const port = process.env.PORT || 3000;
 
-app.listen(port,()=>{
+app.listen(port, () => {
     User.findOne().then(user => {
         if (!user) {
             const user = new User({
