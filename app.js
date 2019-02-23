0de64658bbffdbee00ef2a58bb1c0de595f4aa05
AmrAlmagic <amr.abdalrahman94@gmail.com>
2019-02-23 16:34:27 +0200

    payment with stripe

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ const csrf = require('csurf');
 const flash = require('connect-flash');
 const multer = require('multer');
 
+
 const errorController = require('./controllers/error');
+
+const shopController = require('./controllers/shop');
+const isAuth = require('./middleware/is-auth');
+
 const User = require('./models/user');
 
 const MONGODB_URI = 'mongodb://localhost:27017/shop';
@@ -76,12 +81,11 @@ app.use(
     })
 );
 
-app.use(csrfProtection);
+
 app.use(flash());
 
 app.use((req, res, next) => {
     res.locals.isAuthenticated = req.session.isLoggedIn;
-    res.locals.csrfToken = req.csrfToken();
     next();
 });
 
@@ -103,6 +107,14 @@ app.use((req, res, next) => {
         });
 });
 
+app.post('/create-order',isAuth,shopController.postOrder);
+
+app.use(csrfProtection);
+
+app.use((req, res, next) => {
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
