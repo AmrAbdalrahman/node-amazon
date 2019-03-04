a33e2759832fee384a2c9b4f9f3daf2637096300
AmrAlmagic <amr.abdalrahman94@gmail.com>
2019-03-04 23:37:27 +0200

    compress assets and add morgan

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -9,6 +9,8 @@ const csrf = require('csurf');
 const flash = require('connect-flash');
 const multer = require('multer');
 const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
 
 
 const errorController = require('./controllers/error');
@@ -70,7 +72,13 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'),
+    {flags: 'a'});
+
 app.use(helmet());
+app.use(compression());
+app.use(morgan('combined',{stream: accessLogStream}));
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
@@ -112,7 +120,7 @@ app.use((req, res, next) => {
         });
 });
 
-app.post('/create-order',isAuth,shopController.postOrder);
+app.post('/create-order', isAuth, shopController.postOrder);
 
 app.use(csrfProtection);
 
